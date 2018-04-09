9e1661d2a220cd2c840c119a015262e69a93cb09
Hoai Thu <hoaithuxt1998@gmail.com>
2018-04-09 17:39:26 +0700

    update test servlet

--- src/main/java/com/jcia/jlickr/servlet/TestServlet.java
+++ src/main/java/com/jcia/jlickr/servlet/TestServlet.java
@@ -1,4 +1,17 @@
 package com.jcia.jlickr.servlet;
 
-public class TestServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().append("I'm Thu. Hello Hieu! ");
+
+    }
 }
